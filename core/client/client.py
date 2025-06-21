2456d93092abbdae3f5135d6495fac793c438366
yashkhatri8399 <yash.kuma1810@gmail.com>
2025-06-22 01:06:17 +0530

    Groq changes

--- core/client/client.py
+++ core/client/client.py
@@ -1,1 +1,10 @@
 import langchain
+
+from langchain_groq.chat_models import ChatGroq
+
+model= ChatGroq(
+    model_name="groq/llama-3.1-70b-chat",
+    max_retries=3,
+    max_tokens=4096,
+    temperature=0.1,
+    top_p=0.9,)
