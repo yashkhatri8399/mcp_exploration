99143542b5ebc434376cb5dfcd8d729345c057aa
yashkhatri8399 <yash.kuma1810@gmail.com>
2025-06-22 01:16:44 +0530

    comments

--- core/client/client.py
+++ core/client/client.py
@@ -12,3 +12,15 @@ model= ChatGroq(
 model.invoke(
     "What is the capital of France? Please answer in one sentence.",
 )
+
+# langchain.llms.Groq is deprecated, use langchain_groq.chat_models.ChatGroq instead
+# model = langchain.llms.Groq(
+#     model_name="groq/llama-3.1-70b-chat",        
+#     max_retries=3,
+#     max_tokens=4096,
+#     temperature=0.1,
+#     top_p=0.9,
+# )
+# model.invoke(
+#     "What is the capital of France? Please answer in one sentence.",
+# )
